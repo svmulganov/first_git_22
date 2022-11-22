3a2665eb7ec870e1ca414a7c24464a812586c2b2
mulganov <mulganovsv@yandexlyceum.ru>
2022-11-22 18:14:43 +0300

    второе изменение

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
     print('My first git program')
     print('My git program')
+    print('second change')
 
 
 if __name__ == '__main__':
     main()
+# 0b869d5e3c90f3daff5e366b065221cfdc24db12
